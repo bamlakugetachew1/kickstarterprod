157d32312fb3ae0a53abdbaf1a076a114df8efc9
bamlaku <abuyeget@gmail.com>
2024-05-18 19:02:07 +0300

    using ioredis for redis

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,16 @@
-const redis = require('redis');
+// const redis = require('redis');
+const Redis = require('ioredis');
 const { redisUrl } = require('./env.config');
 
-const client = redis.createClient({ url: redisUrl });
+const client = new Redis(redisUrl);
+
+// const client = redis.createClient({
+//   password: 'vALRVarAbJeWmkE7c50Ij7APaF0wDVPh',
+//   socket: {
+//     host: 'redis-12946.c99.us-east-1-4.ec2.redns.redis-cloud.com',
+//     port: 12946,
+//   },
+// });
+
+// const client = redis.createClient({ url: redisUrl });
 module.exports = client;
