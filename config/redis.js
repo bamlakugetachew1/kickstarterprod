430c2330b04b33b764c51a2200dc0264da701d27
bamlaku <abuyeget@gmail.com>
2024-05-25 22:43:28 +0300

    modeifying redis config

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,7 @@
-// const redis = require('redis');
-const Redis = require('ioredis');
+const redis = require('redis');
 const { redisUrl } = require('./env.config');
 
-const client = new Redis(redisUrl);
-
-// const client = redis.createClient({
-//   password: 'vALRVarAbJeWmkE7c50Ij7APaF0wDVPh',
-//   socket: {
-//     host: 'redis-12946.c99.us-east-1-4.ec2.redns.redis-cloud.com',
-//     port: 12946,
-//   },
-// });
-
-// const client = redis.createClient({ url: redisUrl });
+const client = redis.createClient({
+  url: redisUrl,
+});
 module.exports = client;
